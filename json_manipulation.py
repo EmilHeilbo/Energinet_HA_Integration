f94fbd4184c02c501cc29330d19b0f90cd0121c8
EmilHeilbo <19676610+EmilHeilbo@users.noreply.github.com>
2022-10-07 23:07:36 +0200

    misc(json): add sample return string for testing

---
+++ json_manipulation.py
@@ -0,0 +1,32 @@
+# Sample return from /api/meteringpoints/meteringpoints
+return_test = """
+{
+  "result": [
+    {
+      "streetCode": "0001",
+      "streetName": "REDACTED",
+      "buildingNumber": "1",
+      "floorId": "None",
+      "roomId": "None",
+      "citySubDivisionName": "REDACTED",
+      "municipalityCode": "123",
+      "locationDescription": "Udvendig FRI adgang",
+      "settlementMethod": "D01",
+      "meterReadingOccurrence": "PT1H",
+      "firstConsumerPartyName": "Emil Heilbo",
+      "secondConsumerPartyName": "None",
+      "meterNumber": "4143039",
+      "consumerStartDate": "2001-01-01T00:00:00.000Z",
+      "meteringPointId": "000000000000000001",
+      "typeOfMP": "E17",
+      "balanceSupplierName": "Norlys Energi A/S",
+      "postcode": "1234",
+      "cityName": "REDACTED",
+      "hasRelation": "True",
+      "consumerCVR": "None",
+      "dataAccessCVR": "None",
+      "childMeteringPoints": []
+    }
+  ]
+}
+"""
